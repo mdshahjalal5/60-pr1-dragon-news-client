260422167f520c947e0a8f173f04961e6a6ba3cf
mdshahjalal5 <muhommodshahjalal@gmail.com>
2022-12-31 04:17:10 +0600

    leftside started

--- src/components/LeftSideNav.js
+++ src/components/LeftSideNav.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import { async } from 'q';
+import React, { useEffect, useState } from 'react';
 
 const LeftSideNav = () => {
+    const [categories, setCategories] = useState([]);
+    console.log(categories)
+    const datas = async() =>{
+        const res = await fetch('http://localhost:5000/news-categories');
+        const data = await res.json();
+        setCategories(data);
+    }
+    useEffect(function(){
+        datas();
+    },[])
     return (
         <div>
-            <h2>Left side nav</h2>
+            <h2>All categories {categories.length} </h2>
         </div>
     );
 };
